ef767545dc3c3c15935a5bffff6756a338f8e6aa
alimurtaza18 <alimurt1919@gmail.com>
2026-01-14 22:18:02 +0500

    Passenger account Registration
    I started the work for Passenger account registration and how to validate the inputs.

--- DataStore.cpp
+++ DataStore.cpp
@@ -5,23 +5,53 @@ using namespace std;
 
 class DataStore {
 
+public:
 	void registerUserAccount() {
-		string name, phonenumber, email, userID; 
-		cout << "--------User's Account Registration-------" << endl; 
+		string name, phonenumber, email, userID;
+
+
+		cout << "--------User's Account Registration-------" << endl;
 		cout << "Enter your name: " << endl;
 		cin >> name;
-		cout << "Enter phone number: " << endl; 
-		cin >> phonenumber; 
-		cout << "Enter email address: " << endl; 
-		cin >> email; 
-		
+
+		cout << "Enter phone number: " << endl;
+		cin >> phonenumber;
+		numberValidation(phonenumber); 
+		bool result = numberValidation; 
+		if (result == false) {
+			cout << "Invalid phone number " << endl; 
+			return; 
+		}
+
+		cout << "Enter email address: " << endl;
+		cin >> email;
+
+		ofstream namefile("Person.txt", ios::app);
+		if (namefile.is_open()) {
+			namefile << name << endl;
+		}
+		else {
+			cout << "File can't be opened." << endl;
+		}
+}
+
+	bool numberValidation(string number) {
+		allDigitsCheck(number); 
+		bool result = allDigitsCheck;
+		if (number.length() != 11 || number.substr(0,2) == "03" || result==true) {
+			cout << "Inavlid Number" << endl; 
+			return false; 
+		}
+		return true; 
 	}
 
-	void writetoPersonFile() {
-		ofstream file("Person.txt", ios::app); 
-		if (file.is_open()) {
-
+	bool allDigitsCheck(string number) {
+		for (int i = 0; i < number.length(); i++) {
+			if (!isdigit(number[i])) {
+				return false; 
+			}
 		}
+		return true; 
 	}
 
 };
