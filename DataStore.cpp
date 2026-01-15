3a7f6a041e039d4937f0b052546ab8026c7aac54
alimurtaza18 <alimurt1919@gmail.com>
2026-01-15 23:13:21 +0500

    Number and email validation in passenger account registration
    Today I applied conditions to check if the entered email or phone number is a valid one or not while registering a passengers account

--- DataStore.cpp
+++ DataStore.cpp
@@ -8,36 +8,39 @@ class DataStore {
 public:
 	void registerUserAccount() {
 		string name, phonenumber, email, userID;
+		bool numberresult = isValidNumber;
+		bool emailresult = isValidEmail; 
 
 
 		cout << "--------User's Account Registration-------" << endl;
 		cout << "Enter your name: " << endl;
 		cin >> name;
 
+		while (numberresult != true) {
 		cout << "Enter phone number: " << endl;
 		cin >> phonenumber;
-		numberValidation(phonenumber); 
-		bool result = numberValidation; 
-		if (result == false) {
+		isValidNumber(phonenumber); 
 			cout << "Invalid phone number " << endl; 
-			return; 
 		}
 
-		cout << "Enter email address: " << endl;
-		cin >> email;
+		while (emailresult != true) {
+			cout << "Enter email address: " << endl;
+			cin >> email;
+			isValidEmail(email); 
+		}
 
-		ofstream namefile("Person.txt", ios::app);
-		if (namefile.is_open()) {
-			namefile << name << endl;
+		ofstream Passengerfile("Passenger.txt", ios::app);
+		if (Passengerfile.is_open()) {
+			Passengerfile << name << " " << phonenumber << " " << email << endl;
 		}
 		else {
 			cout << "File can't be opened." << endl;
 		}
 }
 
-	bool numberValidation(string number) {
-		allDigitsCheck(number); 
-		bool result = allDigitsCheck;
+	bool isValidNumber(string number) {
+		areAllDigits(number); 
+		bool result = areAllDigits;
 		if (number.length() != 11 || number.substr(0,2) == "03" || result==true) {
 			cout << "Inavlid Number" << endl; 
 			return false; 
@@ -45,7 +48,7 @@ public:
 		return true; 
 	}
 
-	bool allDigitsCheck(string number) {
+	bool areAllDigits(string number) {
 		for (int i = 0; i < number.length(); i++) {
 			if (!isdigit(number[i])) {
 				return false; 
@@ -54,4 +57,33 @@ public:
 		return true; 
 	}
 
+	bool isValidEmail(string email) {
+		int count = 0; 
+		bool result; 
+		int dotind, atind; 
+		for (int i = 0; i < email.length(); i++) {
+			if (email[i] == '@') {
+				count++; 
+			}
+			if (count != 1) {
+				result = false; 
+			}
+			if (email[0] == '@' || email[email.length() - 1] == '@') {
+				result = false; 
+			}
+			if (email[i] == '.') {
+				dotind = i; 
+			}
+			else if (email[i] == '@') {
+				atind == i; 
+			}
+			if (dotind <= atind) {
+				return false; 
+			}
+			if (email[i] == ' ') {
+				return false; 
+			}
+		} 
+		return true; 
+	}
 };
